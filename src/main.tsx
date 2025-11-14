bed58ae8794d5bae0357f9d6886be33e46d84872
Sargunjit5915 <sargunjit@dussatglobal.com>
2025-11-14 16:50:38 +0530

    Refactor rendering logic in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css"; // ensure this exists
+import "./index.css";
 import DussatInventory from "./DussatInventory";
 
-createRoot(document.getElementById("root")).render(
+const container = document.getElementById("root")!;
+createRoot(container).render(
   <React.StrictMode>
     <DussatInventory />
   </React.StrictMode>
